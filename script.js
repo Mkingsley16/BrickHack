e1931655b15adf9576a4cb24237a521f61b1ad34
Michael Kingsley <mrk2979@g.rit.edu>
2023-02-25 12:58:46 -0500

    basic files

---
+++ script.js
@@ -0,0 +1,23 @@
+function handleSubmit(event) {
+    event.preventDefault(); // prevent the form from submitting
+  
+    // get the input value
+    const input = document.getElementById('input').value;
+  
+    // run the algorithm
+    const output = myAlgorithm(input);
+  
+    // display the output
+    const outputElement = document.getElementById('output');
+    outputElement.innerText = output;
+  }
+  
+  function myAlgorithm(input) {
+    // add your algorithm code here
+    return input.toUpperCase();
+  }
+  
+  // add an event listener to the form
+  const form = document.querySelector('form');
+  form.addEventListener('submit', handleSubmit);
+  
