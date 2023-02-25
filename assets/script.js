e8d95625f08884c6db6e5fbb3da42517b2d4dbab
The_Trapp <arthur.trapp4@gmail.com>
2023-02-25 14:57:11 -0500

    .

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,37 @@
-function handleSubmit(event) {
+
+function handleCeasarSubmit(event) {
     event.preventDefault(); // prevent the form from submitting
   
     // get the input value
-    const input = document.getElementById('input').value;
-  
+    const input = document.getElementById('ceasarinput').value;
+    const shift = document.getElementById('ceasarshift').value;
+
     // run the algorithm
-    const output = encryptWithAES(input);
-  
+    const output = encryptWithCeasar(input,shift);
     // display the output
-    const outputElement = document.getElementById('output');
+    const outputElement = document.getElementById('ceasaroutput');
     outputElement.innerText = output;
   }
   
-  function encryptWithAES(input) {
-    // const CryptoJS = require('crypto-js');
-    // add your algorithm code here
-    const pass = '123';
-    return CryptoJS.AES.encrypt(input, pass);
-  }
+  function encryptWithCeasar(input,shift) {
+      var encryptedMessage = "";
+      for (var i = 0; i < input.length; i++) {
+        var c = input.charCodeAt(i);
+    
+        if (c >= 65 && c <= 90) {
+          encryptedMessage += String.fromCharCode((c - 65 + shift) % 26 + 65); // uppercase letters
+        } else if (c >= 97 && c <= 122) {
+          encryptedMessage += String.fromCharCode((c - 97 + shift) % 26 + 97); // lowercase letters
+        } else {
+          encryptedMessage += message.charAt(i); // non-letter characters
+        }
+      }
+    
+      return encryptedMessage;
+    }
   
   // add an event listener to the form
   const form = document.querySelector('form');
-  form.addEventListener('submit', handleSubmit);
+  form.addEventListener('submit', handleCeasarSubmit);
+
   
